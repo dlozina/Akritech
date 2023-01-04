e6e039104659e413af425849ce8b70c40b5104ba
Dino Lozina <dino.lozina@live.com>
2023-01-04 10:19:06 +0100

    Added first content

---
+++ src/templates/AboveFooter.tsx
@@ -0,0 +1,11 @@
+import { Section } from '../layout/Section';
+
+const AboveFooter = () => (
+  <Section
+    title="Better code"
+    description="Applications deserve better code. There are two reasons for that. One is obvious. Performance. Second is not so obvious since we live in an agile world. We need to maintain what we build. Even when requirements don’t change (highly unlikely 😊), the world and technology changes. Most of the time, developers are reading code. We spend a lot of time onboarding new people to our codebase. That does not have to be an unpleasant experience if we have a good code to work with."
+    >
+  </Section>
+);
+
+export { AboveFooter };
---
+++ src/templates/Quote.tsx
@@ -0,0 +1,13 @@
+import { CTABanner } from '../cta/CTABanner';
+import { Section } from '../layout/Section';
+
+const Quote = () => (
+  <Section>
+    <CTABanner
+      title="The only valid measurement of code quality: WTF/minute"
+      subtitle="Clean Code"
+    />
+  </Section>
+);
+
+export { Quote };
