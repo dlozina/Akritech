f930cc096e0e2614c0220fea3247e6f803da4d35
Dino Lozina <Dino.Lozina@live.com>
2024-04-11 13:28:18 +0200

    change contact email

--- src/templates/Contact.tsx
+++ src/templates/Contact.tsx
@@ -3,7 +3,7 @@ import { Section } from '../layout/Section';
 const Contact = () => (
   <Section title="Contact us">
     <div className="mt-4 text-xl md:px-20 text-center text-primary-500 font-bold">
-      <a href="mailto:dino.lozina@outlook.com">EMAIL</a>
+      <a href="mailto:dino@akritech.eu">EMAIL</a>
     </div>
   </Section>
 );
